30a7984d08b35941f155160974a4b8c5afb783ca
Damely Tineo <damely.tineo07@gmail.com>
2019-12-20 03:48:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,7 @@ def roll_call_dwarves(dwarves)
  end
 end
 
+
 def summon_captain_planet(planeteer_calls)
   return planeteer_calls.map {|call| call.capitalize + "!"}
 end
@@ -12,7 +13,6 @@ end
 def long_planeteer_calls(calls)
  calls.any? {|call| call.size > 4}
 end
-
 
 
 def find_the_cheese (snacks)
